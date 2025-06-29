6dca60371c9dc3d8862d1601a4f856b70bc1902e
Prajwal Arali <147275460+PrajwalArali@users.noreply.github.com>
2025-06-29 23:49:32 +0530

    Update and rename .eslintrc.js to .eslintrc.json

--- .eslintrc.js
+++
@@ -1,8 +0,0 @@
-/** @type {import("eslint").Linter.Config} */
-module.exports = {
-  extends: "next/core-web-vitals",
-  rules: {
-    "@typescript-eslint/no-explicit-any": "off",
-    "@typescript-eslint/no-unused-vars": "off"
-  },
-};
