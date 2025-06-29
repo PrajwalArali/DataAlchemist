86b8e5f2739fc871dc61b53ce1ed5321c8c4c3f0
PrajwalArali <prajwalarali12@gmail.com>
2025-06-30 00:40:42 +0530

    config

---
+++ .eslintrc.js
@@ -0,0 +1,7 @@
+module.exports = {
+  extends: ["next", "next/core-web-vitals", "plugin:@typescript-eslint/recommended"],
+  rules: {
+    "@typescript-eslint/no-explicit-any": "off",    
+    "@typescript-eslint/no-unused-vars": "warn",     
+  },
+};
